9f2aaa3f1a02eab49e919cd02a6e70f7351191fb
fengtiecheng <17615007230@163.com>
2025-07-30 17:26:33 +0800

    feat(dao/generator_test): 添加数据库连接初始化和关闭逻辑
    - 在 `Test_generate1` 测试函数中添加数据库连接初始化和关闭逻辑
    - 增加日志记录以确认数据库连接已关闭

--- dao/generator_test.go
+++ dao/generator_test.go
@@ -3,17 +3,31 @@ package dao
 
 import (
 	"gorm.io/gorm"
+	"log"
 	"testing"
 )
 
 func Test_generate1(t *testing.T) {
+
+	//1.初始化DB
 	InitDB()
+
+	//2.确保最终关闭数据库链接
+	defer func() {
+		db, _ := DB.DB()
+		_ = db.Close()
+		log.Printf("database connection closed")
+	}()
+
+	//3.定义参数结构体
 	type args struct {
 		db           *gorm.DB
 		tables       []string
 		outPath      string
 		modelPkgPath string
 	}
+
+	//4.定义测试用例
 	tests := []struct {
 		name string
 		args args
@@ -28,6 +42,8 @@ func Test_generate1(t *testing.T) {
 			},
 		},
 	}
+
+	//5.执行测试（也就是生成代码）
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			generate(tt.args.db, tt.args.tables, tt.args.outPath, tt.args.modelPkgPath)
